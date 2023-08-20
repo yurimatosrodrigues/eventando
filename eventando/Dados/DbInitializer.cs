edc8cbfa9962385f967f30e770feb81948bcfb95
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2023-08-20 16:38:22 -0300

    Criando context, injeção de dependência e banco de dados

---
+++ eventando/Dados/DbInitializer.cs
@@ -0,0 +1,10 @@
+﻿namespace eventando.Dados
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(EventosContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
--- eventando/Program.cs
+++ eventando/Program.cs
@@ -1,9 +1,32 @@
+using eventando.Dados;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<EventosContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("EventosConnection"))
+);
 
 var app = builder.Build();
+
+//Configurando injeção de dependência
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<EventosContext>();
+        DbInitializer.Initialize(context); //cria o BD caso não exista
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger>();
+        logger.LogError(ex, ex.Message);
+        throw;
+    }
+};
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
